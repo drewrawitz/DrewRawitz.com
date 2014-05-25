704d3aeeadee107b388cce3444a4235c936edcb6
Drew Rawitz <drew.rawitz@atlanticbt.com>
2014-05-25 02:36:13 -0400

    split everything up into a src/public folder - and using gulp to copy the files over

--- gulpfile.js
+++ gulpfile.js
@@ -10,77 +10,119 @@ var autoprefixer = require('gulp-autoprefixer'),
     rename = require('gulp-rename'),
     concat = require('gulp-concat'),
     notify = require('gulp-notify'),
-    cache = require('gulp-cache'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    newer = require('gulp-newer'),
+    clean = require('gulp-clean'),
+    spritesmith = require('gulp.spritesmith');
 
 // Define some project variables
-var projectApp = 'public',
-    projectCSS = projectApp + '/assets/css',
-    projectSASS = projectApp + '/assets/scss',
-    projectJS = projectApp + '/assets/js',
-    projectImages = projectApp + '/assets/images';
+var destApp = 'public',
+    srcApp = 'src',
+    destCSS = destApp + '/assets/css',
+    destJS = destApp + '/assets/js',
+    destImages = destApp + '/assets/images',
+    srcSASS = srcApp + '/assets/scss',
+    srcJS = srcApp + '/assets/js',
+    srcImages = srcApp + '/assets/images';
+
+// Define my src files
+function srcFiles(path) {
+  var srcFiles = [
+    path+'/.htaccess',
+    path+'/*.php',
+    path+'/assets/font/**',
+    path+'/libs/**',
+    path+'/scripts/**',
+    path+'/includes/**',
+    path+'/robots.txt',
+    path+'/sitemap.xml'
+  ];
+
+  return srcFiles;
+}
 
 // Styles task
 gulp.task('styles', function() {
-    return gulp.src(''+projectSASS+'/styles.scss')
+    return gulp.src(''+srcSASS+'/styles.scss')
       .pipe(sass({ style: 'expanded' }))
       .pipe(autoprefixer('last 2 version'))
       .pipe(minifycss())
-      .pipe(gulp.dest(projectCSS))
+      .pipe(gulp.dest(destCSS))
       .pipe(notify({ message: 'Styles task complete' }));
 });
 
 // Scripts task
 gulp.task('scripts', function() {
     return gulp.src([
-        ''+projectJS+'/plugins/*.js',
-        ''+projectJS+'/main.js'
+        ''+srcJS+'/plugins/*.js',
+        ''+srcJS+'/main.js'
       ])
       .pipe(concat('scripts.min.js'))
       .pipe(uglify())
-      .pipe(gulp.dest(projectJS))
+      .pipe(gulp.dest(destJS))
       .pipe(notify({ message: 'Scripts task complete' }));
 });
 
 // Images task
 gulp.task('images', function() {
-  return gulp.src(''+projectImages+'/**/*')
-    .pipe(cache(imagemin({ optimizationLevel: 8, progressive: true, interlaced: true })))
-    .pipe(gulp.dest(projectImages))
-    .pipe(notify({ message: 'Images task complete' }));
+  return gulp.src(srcImages+'/**/*')
+      .pipe(newer(destImages))
+      .pipe(imagemin())
+      .pipe(gulp.dest(destImages));
+});
+
+// Copy files from src to public
+gulp.task('copy', ['cleanApp'], function() {
+  return gulp.src(srcFiles(srcApp), { base: './src' })
+    .pipe(gulp.dest(destApp))
+});
+
+// Clean out the public folder
+gulp.task('cleanApp',function() {
+  return gulp.src(srcFiles(destApp))
+    .pipe(clean())
 });
 
 // Sprite Generator task
 gulp.task('sprite', function () {
-  var spriteData = gulp.src('public/assets/images/technologies/*.png').pipe(spritesmith({
-    imgName: 'sprite.png',
-    cssName: '_sprite.scss',
-    cssFormat: 'scss'
+  var spriteData = gulp.src(srcImages+'/technologies/*.png').pipe(spritesmith({
+    imgName: 'technologies-sprite.png',
+    cssName: 'partials/_technologies.scss',
+    cssFormat: 'css',
+    imgPath: '../images/technologies-sprite.png',
+    cssOpts: {
+      cssClass: function (item) {
+        return '.technologies-' + item.name;
+      }
+    }
   }));
-  spriteData.img.pipe(gulp.dest(projectImages+'/'));
-  spriteData.css.pipe(gulp.dest(projectSASS+'/'));
+  spriteData.img.pipe(gulp.dest(srcImages+'/'));
+  spriteData.css.pipe(gulp.dest(srcSASS+'/'));
 });
 
 
 // Default task
-gulp.task('default', ['styles', 'scripts', 'images']);
+gulp.task('default', ['styles', 'scripts', 'images', 'copy']);
 
 // Watch
 gulp.task('watch', function() {
   // Watch .scss files
-  gulp.watch(''+projectSASS+'/**/*.scss', ['styles']);
+  gulp.watch(''+srcSASS+'/**/*.scss', ['styles']);
 
   // Watch .js files
-  gulp.watch(''+projectJS+'/**/*.js', ['scripts']);
+  gulp.watch(''+srcJS+'/**/*.js', ['scripts']);
+
+  // Watch PHP files
+  gulp.watch(srcFiles(srcApp), ['copy']);
 
   // Watch image files
-  //gulp.watch(''+projectImages+'/**/*', ['images']);
+  gulp.watch(''+srcImages+'/**/*', ['images']);
 
   // Create LiveReload server
   var server = livereload();
 
   // Watch any files in dist/, reload on change
-  gulp.watch([''+projectApp+'/**']).on('change', function(file) {
+  gulp.watch([''+srcApp+'/**']).on('change', function(file) {
     server.changed(file.path);
   });
 });
