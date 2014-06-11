5677ef14f3080f89861c24793aa65a3289a0600e
Drew Rawitz <drew.rawitz@atlanticbt.com>
2014-06-10 22:53:39 -0400

    cleaning up gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -16,7 +16,7 @@ var autoprefixer = require('gulp-autoprefixer'),
     spritesmith = require('gulp.spritesmith'),
     runSequence = require('run-sequence'),
     revHash = require('gulp-rev-hash'),
-    rsync = require('rsyncwrapper').rsync;
+    rsync = require('rsyncwrapper').rsync,
     secrets = require('./secrets.json');
 
 // Define some project variables
@@ -47,10 +47,9 @@ function srcFiles(path) {
 
 // Styles task
 gulp.task('styles', function() {
-    return gulp.src(''+srcSASS+'/styles.scss')
+    return gulp.src(srcSASS+'/styles.scss')
       .pipe(sass({ style: 'compact', sourcemap: true }))
       .pipe(autoprefixer('last 2 version'))
-      //.pipe(minifycss())
       .pipe(gulp.dest(destCSS))
       .pipe(notify({ message: 'Styles task complete' }));
 });
@@ -94,6 +93,14 @@ gulp.task('rev-hash', function () {
         .pipe(gulp.dest(destApp));
 });
 
+// Minify our CSS
+gulp.task('minify', function() {
+    return gulp.src(destCSS+'/styles.css')
+      .pipe(minifycss())
+      .pipe(gulp.dest(destCSS))
+      .pipe(notify({ message: 'Minification task complete' }));
+});
+
 // Sprite Generator task
 gulp.task('sprite', function () {
   var spriteData = gulp.src(srcImages+'/technologies/*.png').pipe(spritesmith({
@@ -129,29 +136,29 @@ gulp.task('deploy', function() {
 });
 
 // Default task
-gulp.task('default', function(cb) {
-  runSequence(['styles', 'scripts', 'images'],'copy','rev-hash',cb);
+gulp.task('build', function(cb) {
+  runSequence(['styles', 'scripts', 'images'],'copy','rev-hash', 'minify',cb);
 });
 
 // Watch
 gulp.task('watch', function() {
   // Watch .scss files
-  gulp.watch(''+srcSASS+'/**/*.scss', ['styles', 'rev-hash']);
+  gulp.watch(srcSASS+'/**/*.scss', ['styles', 'rev-hash']);
 
   // Watch .js files
-  gulp.watch(''+srcJS+'/**/*.js', ['scripts']);
+  gulp.watch(srcJS+'/**/*.js', ['scripts']);
 
   // Watch PHP files
   gulp.watch(srcFiles(srcApp), ['copy']);
 
   // Watch image files
-  gulp.watch(''+srcImages+'/**/*', ['images']);
+  gulp.watch(srcImages+'/**/*', ['images']);
 
   // Create LiveReload server
   var server = livereload();
 
   // Watch any files in dist/, reload on change
-  gulp.watch([''+srcApp+'/**']).on('change', function(file) {
+  gulp.watch([srcApp+'/**']).on('change', function(file) {
     server.changed(file.path);
   });
 });
